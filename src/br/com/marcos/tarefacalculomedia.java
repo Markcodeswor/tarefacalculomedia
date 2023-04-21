f1f0d7e3b53b5071f7579ae1ce4a43bd9c76919e
Markcodeswor <113196496+marcos-de-santana@users.noreply.github.com>
2023-04-21 13:12:10 -0300

    Initial commit

---
+++ src/br/com/marcos/tarefacalculomedia.java
@@ -0,0 +1,23 @@
+package br.com.marcos;
+
+public class tarefacalculomedia {
+    public static void main (String args[]) {
+
+        int num1 = 10;
+        int num2 = 9;
+        int num3 = 7;
+        int num4 = 6;
+
+        int num5 = 10 + 9 + 7 + 6;
+        int soma = num5 / 4;
+
+
+        System.out.print("A soma dos números é  " + num5);
+        System.out.print("***Agora basta dividir a soma do resultado, por 4 ***");
+        System.out.print("O resultado da média desses 4 números é igual a.. " + soma);
+          }
+
+
+
+
+    }
